52cd16e3650d558db1f52db3b41f534ef215d203
amber3905 <amberbetts2005@gmail.com>
2026-04-18 15:23:18 +0100

    Located each job card

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-x = requests.get("https://realpython.github.io/fake-jobs/")
+x = requests.get("https://realpython.github.io/fake-jobs/", timeout=1)
 y = BeautifulSoup(x.content, "html.parser")
+z = y.find_all(True, class_="card-content")
 
-print(y.prettify())
+print(z)
