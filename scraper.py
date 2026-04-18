208e7bbc1d4228cb7f26602ef30ea1b817b25938
amber3905 <amberbetts2005@gmail.com>
2026-04-18 15:47:31 +0100

    Extracted Title, Company and Location

--- scraper.py
+++ scraper.py
@@ -4,5 +4,9 @@ from bs4 import BeautifulSoup
 x = requests.get("https://realpython.github.io/fake-jobs/", timeout=1)
 y = BeautifulSoup(x.content, "html.parser")
 z = y.find_all(True, class_="card-content")
-
-print(z)
+for job in z:
+    title = job.find("h2").get_text(strip=True)
+    company = job.find("h3").get_text(strip=True)
+    location = job.find("p",class_="location").get_text(strip=True)
+    url = 0
+    print("Title:", title+ ", Company:", company+ ", Location:", location)
