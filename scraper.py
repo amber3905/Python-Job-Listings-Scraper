3206e1feb8481edd15701e2a3808fdad60df6d0a
amber3905 <amberbetts2005@gmail.com>
2026-04-19 19:08:22 +0100

    Extracted URL for detailed job pages

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 x = requests.get("https://realpython.github.io/fake-jobs/", timeout=1)
 y = BeautifulSoup(x.content, "html.parser")
@@ -8,5 +9,5 @@ for job in z:
     title = job.find("h2").get_text(strip=True)
     company = job.find("h3").get_text(strip=True)
     location = job.find("p",class_="location").get_text(strip=True)
-    url = 0
-    print("Title:", title+ ", Company:", company+ ", Location:", location)
+    url = job.find("a", href=re.compile("jobs"))
+    print("Title:", title + ", Company:", company + ", Location:", location + ", Url:", url.get("href"))
